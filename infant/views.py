379ba105701e3b7ca4570d992c765ca8d94c7636
Ling Su <sl_62469430@unknown705681a6435f.att.net>
2013-05-13 23:37:26 -0500

    added auth

--- infant/views.py
+++ infant/views.py
@@ -1,1 +1,16 @@
+from django.shortcuts import render,redirect
+
+from infant.models import diapering,nap,feedingS,feedingL
+
+from retro.forms import diapperingForm,napForm,feedingSForm,feedingLForm
+
+from django.contrib.auth.decorators import login_required
+
+@login_required(login_url='/')
+def activitySelection(request):
+  if request.method=='POST':
+    if form.is_valid():
+      
+
+
 # Create your views here.
