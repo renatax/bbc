55537df930c1088139baa6240ca933f5b6b87ec6
jgmao <jgmao@hotmail.com>
2013-07-30 23:11:41 -0500

    modify the models: add Person which is 1-to-1 to User add Toy which is an Item change all the users to Person objects

--- infant/models.py
+++ infant/models.py
@@ -1,47 +1,72 @@
 from django.db import models
+from django.contrib.auth.models import User, UserManager
 import datetime
 
 
+class Person(models.Model):
+	GENDER_CHOICE = (('M', 'Male'), ('F', 'Female'),)
+	gender = models.CharField(max_length=1, default='M')
+	birthday = models.DateField(auto_now=True)
+	user = models.OneToOneField(User)
+
 
 class Action(models.Model):
-  user = models.CharField(max_length=20)
-  infant = models.CharField(max_length=20) 
-  class Meta:
-    abstract = True
-  def __unicode__(self):
-    return str(self.pk)
+	creator = models.ForeignKey(Person, related_name='+', null=True)
+	receiver = models.ForeignKey(Person, related_name='+', null=True)
 
-class Food(models.Model):
-    CHOICE = (('S','Solid Food'),('L', 'Liquid Food'))
-    UNITS = (('Oz','Ounce'),('Jar','Jar'),('Pc','Piece'))
-    name = models.CharField(max_length=30)
-    unit = models.CharField(max_length=20,choices=UNITS)
-    food_type = models.CharField(max_length=20, choices = CHOICE)
+	class Meta:
+		abstract = True
+
+	def __unicode__(self):
+		return str(self.pk)
+
+
+class Item(models.Model):
+	name = models.CharField(max_length=30)
+	description = models.TextField(max_length=300)
+
+	class Meta:
+		abstract = True
+
+	def __unicode__(self):
+		return str(self.name)+': '+str(self.description)
+
+
+class Toy(Item):
+	toy_type = models.CharField(max_length=20)
+
+
+class Food(Item):
+	CHOICE = (('S', 'Solid Food'), ('L', 'Liquid Food'))
+	UNITS = (('Oz', 'Ounce'), ('Jar', 'Jar'), ('Pc', 'Piece'))
+	unit = models.CharField(max_length=20, choices=UNITS)
+	food_type = models.CharField(max_length=20, choices=CHOICE)
+
 
 class Feeding(Action):
-  helper = models.CharField(max_length=20,default=None)
-  help_type = models.CharField(max_length=20,default=None)
-  time=models.DateTimeField(default=datetime.datetime.now)
-  amount = models.DecimalField(max_digits=5, decimal_places=2)  
+	helper = models.ForeignKey(Person, related_name='helper', null=True)  # extra helper if any
+	# help_type = models.OneToOneField(Person)
+	time = models.DateTimeField(default=datetime.datetime.now)
+	amount = models.DecimalField(max_digits=5, decimal_places=2)
 
 
 class FeedingLiquids(Feeding):
-  food = models.ForeignKey(Food,limit_choices_to={'food_type':'L'})
+	food = models.ForeignKey(Food, limit_choices_to={'food_type': 'L'})
+
 
 class FeedingSolids(Feeding):
-  food = models.ForeignKey(Food,limit_choices_to={'food_type':'S'})
-  
+	food = models.ForeignKey(Food, limit_choices_to={'food_type': 'S'})
+
+
 class Nap(Action):
-  startAt=models.DateTimeField(default=datetime.datetime.now)
-  endAt=models.DateTimeField(default=datetime.datetime.now)
+	startAt = models.DateTimeField(default=datetime.datetime.now)
+	endAt = models.DateTimeField(default=datetime.datetime.now)
+
+
 class Diapering(Action):
-  CHOICES = (
-        (True, 'Yes'),
-        (False, 'No'),
-           )
-  ointment=models.BooleanField(choices = CHOICES, default = False)
-  bm =models.BooleanField(choices = CHOICES, default = False) 
-  wet = models.BooleanField(choices = CHOICES, default = False)
+	CHOICES = ((True, 'Yes'), (False, 'No'),)
+	ointment = models.BooleanField(choices=CHOICES, default=False)
+	bm = models.BooleanField(choices=CHOICES, default=False)
+	wet = models.BooleanField(choices=CHOICES, default=False)
 
 
-# Create your models here.
